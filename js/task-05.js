6556a4a5567c9225d1b32580142eb609adc2b10b
Andrii-Posternak <panposternak@gmail.com>
2022-12-11 08:43:06 +0200

    fixed 5 task

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,10 @@ const inputRef = document.querySelector('input#name-input');
 const outputRef = document.querySelector('span#name-output');
 
 const getInputValue = event => {
-  const defaultValue = 'Anonymous111';
+  const defaultValue = 'Anonymous';
   if (event.currentTarget.value === '') {
     outputRef.textContent = defaultValue;
+    return;
   }
   outputRef.textContent = event.currentTarget.value;
 };
